94a5ac6af4ffd4d177c0523c5790d0c276750d4b
SnelleJelle05 <152658871+SnelleJelle05@users.noreply.github.com>
2024-12-04 21:09:16 +0100

    now useses function for better code.

--- src/Controller/ScraperController.php
+++ src/Controller/ScraperController.php
@@ -2,6 +2,7 @@
 
    namespace App\Controller;
 
+   use GuzzleHttp\Exception\GuzzleException;
    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
    use GuzzleHttp\Client;
    use Symfony\Component\DomCrawler\Crawler;
@@ -9,34 +10,68 @@
    class ScraperController extends AbstractController
    {
 
+      /**
+       * @throws GuzzleException
+       */
       public function Scrape($response): array
       {
          foreach ($response as $article) {
             $url = $article['url'];
-            $client = new Client();
 
-            $response = $client->request('GET', $url);
+            $title = $this->crawlWebsiteTag($url, 'title');
 
-            $html = $response->getBody()->getContents();
-            $crawler = new Crawler($html);
-            dump($crawler);
-            $title = $crawler->filter('title')->count() ? $crawler->filter('title')->text() : null;
+            $description = $this->crawlWebsiteMetaName($url, 'description');
 
-            $description = $crawler->filter('meta[name="description"]')->count()
-                ? $crawler->filter('meta[name="description"]')->attr('content') : null;
+            $source = $this->crawlWebsiteMetaName($url, 'application-name');
 
-            $source = $crawler->filter('meta[name="application-name"]')->count()
-                ? $crawler->filter('meta[property="application-name"]')->attr('content') : null;
+            $imageUrl = $this->crawlWebsiteMetaProperty($url, 'og:image');
 
-            $imageUrl = $crawler->filter('meta[property="og:image"]')->count()
-                ? $crawler->filter('meta[property="og:image"]')->attr('content') : null;
-
-            $dateTime = $crawler->filter('meta[property="article:published_time"]')->count()
-                ? $crawler->filter('meta[property="article:published_time"]')->attr('content') : null;
+            $dateTime = $this->crawlWebsiteMetaProperty($url, 'article:published_time');
 
             $array[] = ['Title' => $title, 'Description' => $description, 'Source' => $source, 'ImageUrl' => $imageUrl, 'Date' => $dateTime];
          }
          return $array;
+      }
+
+
+
+      /**
+       * @throws GuzzleException
+       */
+      private function crawlWebsiteMetaName($url, $object): ?string
+      {
+         $client = new Client();
+
+         $response = $client->request('GET', $url);
+
+         $html = $response->getBody()->getContents();
+         $crawler = new Crawler($html);
+
+         return $crawler->filter("meta[name='{$object}']")->count()
+             ? $crawler->filter("meta[name='{$object}']")->attr('content') : null;
+      }
+
+      private function crawlWebsiteMetaProperty($url, $object): ?string
+      {
+         $client = new Client();
+
+         $response = $client->request('GET', $url);
+
+         $html = $response->getBody()->getContents();
+         $crawler = new Crawler($html);
+
+         return $crawler->filter("meta[property='{$object}']")->count()
+             ? $crawler->filter("meta[property='{$object}']")->attr('content') : null;
+      }
+
+      private function crawlWebsiteTag($url, $object): ?string
+      {
+         $client = new Client();
+         $response = $client->request('GET', $url);
+         $html = $response->getBody()->getContents();
+         $crawler = new Crawler($html);
+
+         return $crawler->filter($object)->count() ? $crawler->filter($object)->text() : null;
 
       }
 
